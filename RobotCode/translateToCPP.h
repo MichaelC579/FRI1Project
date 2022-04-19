1619ef540a73df4638d0fa16f840b93e3ae5272f
vudu2007 <98369275+vudu2007@users.noreply.github.com>
2022-04-19 15:57:07 -0500

    Added files for translating custom to cpp

---
+++ RobotCode/translateToCPP.h
@@ -0,0 +1,8 @@
+#ifndef TRANSLATE_TO_CPP
+#define TRANSLATE_TO_CPP
+
+int moveForward() {}
+// While loop -> start on position of condition, evaluate, 
+// if true, then go through instructions else, go to end bracket and stop
+
+#endif
