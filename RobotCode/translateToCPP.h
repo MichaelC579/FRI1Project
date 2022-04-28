d5fecf62d42a4a80c9487f9cc5895ac071d560bc
Dinesh Polisetty <dineshsp2003@gmail.com>
2022-04-28 16:35:05 -0500

    Delete translateToCPP.h

--- RobotCode/translateToCPP.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSLATE_TO_CPP
-#define TRANSLATE_TO_CPP
-
-int moveForward() {}
-// While loop -> start on position of condition, evaluate, 
-// if true, then go through instructions else, go to end bracket and stop
-
-#endif
